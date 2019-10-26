22ed8cbcf6c6c3a9366b428307feb2dc5ed1f277
haved <krogstie.havard@gmail.com>
2019-10-26 03:25:46 +0200

    Improve text

--- src/components/GuideText.vue
+++ src/components/GuideText.vue
@@ -4,7 +4,7 @@
       <h2>Graphviz</h2>
       This website lets you visualize different graph based datastructures, and visually see the algorithms operating on the graphs.<br>
       At the top of the page you can choose the data structure of your choice.<br>
-      In this sidebar, there are 3 panels that give information about the current datastructure.
+      At the top of this sidebar there are 3 panels that give information about the current datastructure.
       <h3><font-awesome-icon icon="angle-right" size="sm"/> Node type</h3>
       This panel shows what data each node holds.
       Hovering over a specific node shows the values in that particular node.
@@ -14,6 +14,7 @@
       <h3><font-awesome-icon icon="angle-right" size="sm"/> Algorithms</h3>
       This panel shows functions that can be done on the graph.
       Click one to expand it, and click again to put it on the execute-bar.
+      Here you can also see the algorithm as it executes.
     </div>
   </div>
 </template>
